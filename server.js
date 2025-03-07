ace91692773de4a884060033fd38bce239b74a84
seetler <jsnzeng@gmail.com>
2025-03-07 09:39:59 -0800

    wbuild512

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3512;
 
 // Serve static files from the React build folder
 app.use(express.static(path.join(__dirname, 'build')));
